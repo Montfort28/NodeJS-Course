95d2ca44c6e715f17fcd90a6a3c98fa4016b5ba2
Montfort <mugishamontfort28@gmail.com>
2025-09-20 00:43:45 +0200

    event emmitter

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var stuff= require('./stuff')
 console.log(stuff.counter(['1', '2', '4']))
 console.log(stuff.pi)
 console.log(stuff.adder(5, stuff.pi))
+
+var events = require('events')
+
+var myEmitter = new events.EventEmitter()
+
+myEmitter.on('someEvent', function(mssg){
+    console.log(mssg)
+})
+
+myEmitter.emit('someEvent', 'event was emmitted')
