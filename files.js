c4bf6f9d4b55a48ddb39933b25f49abf91f0bff0
Montfort <mugishamontfort28@gmail.com>
2025-09-21 00:05:46 +0200

    learning how to create directories, delete, read and write files using nodejs

--- files.js
+++ files.js
@@ -10,6 +10,40 @@ const fs = require('fs')
 
 //writing files
 
-fs.writeFile('./doc/blog.txt', 'other blog details added', () =>{
-    console.log('file rewritten')
-})
+// fs.writeFile('./doc/blog.txt', 'other blog details added', () =>{
+//     console.log('file rewritten')
+// })
+
+//directories
+// if(!fs.existsSync('./assets')){
+//   fs.mkdir('./assets', (err) =>{
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log('folder created')
+// })
+// } else{
+//     fs.rmdir('./assets', (err) =>{
+//          if (err) {
+//             console.log(err)
+//          }
+//          console.log('folder deleted')
+//     })
+// }
+
+//deleting files
+if(fs.existsSync('./doc/deleteMe.txt')){
+    fs.unlink('./doc/deleteMe.txt', (err) =>{
+       if(err){
+        console.log(err)
+       }
+       console.log('file deleted successfully')
+    })
+}else{
+    fs.link('./doc/deleteMe.txt', (err) =>{
+        if(err){
+            console.log(err)
+        }
+        console.log('file created successfully')
+    })
+}
