326120128e199ff5d998d3235d5a122aba399707
Montfort <mugishamontfort28@gmail.com>
2025-09-27 00:12:04 +0200

    responses

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) =>{
-   console.log('request made')
-})
+const server = http.createServer((req, res) => {
+  console.log(req);
 
-server.listen(3000, 'localhost', () =>{
-    console.log('listening for request on port 3000')
-})
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+
+    case "/about":
+      path += "about.html";
+      break;
+
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end;
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port 3000");
+});
