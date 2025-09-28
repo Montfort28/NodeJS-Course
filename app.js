8389f9b389b7494fcfa324e6f944c28193c9df16
Montfort <mugishamontfort28@gmail.com>
2025-09-29 00:06:06 +0200

    redirecting routes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.listen(3000)
+app.listen(3000);
 
-app.get('/', (req, res) =>{
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 
-    res.sendFile('./views/index.html', {root: __dirname})
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
 
-})
-
-app.get('/about', (req, res) =>{
-
-    res.sendFile('./views/about.html', {root: __dirname})
-
-})
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
