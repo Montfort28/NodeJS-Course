8bcab429299b11aa4c24ff7c8350b0cbfb5f2764
Montfort <mugishamontfort28@gmail.com>
2025-09-29 00:16:52 +0200

    status codes using express

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get("/about-us", (req, res) => {
 });
 
 app.use((req, res) =>{
-    res.sendFile("./views/404.html", { root: __dirname })
+    res.status(404).sendFile("./views/404.html", { root: __dirname })
 })
