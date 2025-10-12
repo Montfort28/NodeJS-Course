31cc08a7cb6c9e751198a1a1c1f5ab870c695551
Montfort <mugishamontfort28@gmail.com>
2025-10-12 23:05:06 +0200

    trying out new schemas

--- models/blog.js
+++ models/blog.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    title: { type: string, required: true},
+    body: { type: string, required: true},
+    snippet: {type: string, required: true}
+}, { timestamp: true })
