390f8f471e86b5d7fd2b8c07821451fa576d32c4
Montfort <mugishamontfort28@gmail.com>
2025-10-12 23:52:20 +0200

    adding a new blog to the database

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
-const express = require('express');
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 const app = express();
 
-const dbURI = 'mongodb+srv://montfort:1234montfort@cluster0.xuqpcpf.mongodb.net/node-course?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(dbURI)
-.then(() =>
-  app.listen(3000))
-.catch((err) => 
-  console.log(err))
+const dbURI =
+  "mongodb+srv://montfort:1234montfort@cluster0.xuqpcpf.mongodb.net/node-course?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
-app.use(express.static('public'))
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "my new blog",
+    body: "about my new blog",
+    snippet: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.use(morgan('dev'))
-
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
   ];
-  res.render('index', { title: 'Home', blogs });
+  res.render("index", { title: "Home", blogs });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
--- models/blog.js
+++ models/blog.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
-    title: { type: string, required: true},
-    body: { type: string, required: true},
-    snippet: {type: string, required: true}
-}, { timestamp: true })
+    title: { type: String, required: true},
+    body: { type: String, required: true},
+    snippet: { type: String, required: true}
+}, { timestamps: true })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
